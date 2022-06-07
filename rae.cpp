133e02ff794658e07e8c4b3b87b6be605897b52e
mcguireraechel <79418928+mcguireraechel@users.noreply.github.com>
2022-06-07 13:22:59 -0700

    Add files via upload

---
+++ rae.cpp
@@ -0,0 +1,46 @@
+//============================================================================
+// Name        : getting.cpp
+// Author      : Raechel McGuire
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+
+ int *poop(int a, int b,int *lol){
+
+	*lol = a +b;
+
+	return lol;
+
+
+}
+
+
+
+int main() {
+
+	int a =2;
+	int b = 5;
+	int c =18;
+
+	int *r = &c;
+
+	cout<< "C started as" << c << endl;
+
+	poop(a, b, r);
+	cout<< "C ended as" << c << endl;
+
+
+}
+
+
+
+
